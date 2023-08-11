17edcbfa8fe4a897b1e49a40d8abf019a4b8da59
TRICKZTRRRRR <jonas04@outlook.dk>
2023-08-11 12:54:53 +0200

    Tilføjede Program.cs

--- Terning-Dice/Program.cs
+++ Terning-Dice/Program.cs
@@ -10,6 +10,39 @@ namespace Terning_Dice
     {
         static void Main(string[] args)
         {
+            Random number = new Random();
+
+            while (true) 
+            {
+                int dice = number.Next(1, 7);
+
+                Console.Clear();
+                if (dice == 1)
+                {
+                    Console.WriteLine("You hit 1");
+                }
+                else if (dice == 2)
+                {
+                    Console.WriteLine("You hit 2");
+                }
+                else if (dice == 3)
+                {
+                    Console.WriteLine("You hit 3");
+                }
+                else if (dice == 4)
+                {
+                    Console.WriteLine("You hit 4");
+                }
+                else if (dice == 5)
+                {
+                    Console.WriteLine("You hit 5");
+                }
+                else
+                {
+                    Console.WriteLine("You hit 6");
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
