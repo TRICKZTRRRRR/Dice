c86f6d8ae05b1d6933f83eff9c4ff50a2e9a3639
TRICKZTRRRRR <jonas04@outlook.dk>
2023-08-11 13:13:12 +0200

    Tilføjede kommentare og farver

--- Terning-Dice/Program.cs
+++ Terning-Dice/Program.cs
@@ -12,33 +12,43 @@ namespace Terning_Dice
         {
             Random number = new Random();
 
+            Console.Title = "Dice";
+
             while (true) 
             {
                 int dice = number.Next(1, 7);
 
+                Console.BackgroundColor = ConsoleColor.Black;
                 Console.Clear();
+
                 if (dice == 1)
                 {
+                    Console.BackgroundColor = ConsoleColor.Green;
                     Console.WriteLine("You hit 1");
                 }
                 else if (dice == 2)
                 {
+                    Console.BackgroundColor = ConsoleColor.Red;
                     Console.WriteLine("You hit 2");
                 }
                 else if (dice == 3)
                 {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("You hit 3");
                 }
                 else if (dice == 4)
                 {
+                    Console.BackgroundColor = ConsoleColor.Blue;
                     Console.WriteLine("You hit 4");
                 }
                 else if (dice == 5)
                 {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("You hit 5");
                 }
                 else
                 {
+                    Console.BackgroundColor = ConsoleColor.Magenta;
                     Console.WriteLine("You hit 6");
                 }
                 Console.ReadLine();
